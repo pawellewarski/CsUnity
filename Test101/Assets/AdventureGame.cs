26e63da422dcd0044c507eec96e6ec6b4624f1bb
Paweł Lewarski <pawel.lewarski@akmf.pl>
2019-10-23 10:54:47 +0200

    Dodano projekt + lekcja 26

---
+++ Test101/Assets/AdventureGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+
+    State state;
+   
+    void Start() {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        ManageState();
+    }
+}
